c17127f1bb53555d3b49c9fd599115fe84cb837a
Alessia Bellisario <github@bellisar.io>
2019-03-25 19:44:07 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+use std::process;
+
+use minigrep;
+// use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Query is: {}", config.query);
+    println!("Filename is: {}", config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
